e7dd5cd249a9ef9b954ff2bd91e7d477c0924068
claytongulick <claytongulick@gmail.com>
2019-08-29 14:52:23 -0500

    initial commit

---
+++ test/test_pgproxy.js
@@ -0,0 +1,226 @@
+let {Client} = require('pg');
+let assert = require('assert');
+
+//replace cn information with your dev db
+let client = new Client({
+    host: '172.17.174.71',
+    user: 'postgres',
+    password: 'asdf1234',
+    database: 'test'
+});
+
+let PGProxy = require('../lib');
+
+describe('PGProxy Tests', () => {
+
+    before(async () => {
+        await client.connect();
+        //await client.query('drop schema if exists test');
+        //await client.query('create schema test');
+    });
+
+    it('should create new functions', async () => {
+        let functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select '${param1}' as param1, '${param2}' as param2`);
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        let proxy = await PGProxy.create(functions, {client: client, schema: 'test'});
+        let results = await client.query(`select proname, prosrc from pg_proc where proname like 'pgproxy_%'`);
+        let procs = results.rows;
+        let found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_select_test');
+        assert.notEqual(found, null);
+
+        found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_system_name');
+        assert.notEqual(found, null);
+
+    });
+
+    it('should execute and return correct values', async () => {
+        let functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select '${param1}' as param1, '${param2}' as param2`);
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        let proxy = await PGProxy.create(functions, {client: client, schema: 'test'});
+        let row = await proxy.select_test('asdf','1234');
+        assert.equal(row.param1, 'asdf');
+        assert.equal(row.param2, '1234');
+
+        let name = await proxy.system_name("This is A TEST");
+        assert.equal(name, 'this_is_a_test');
+    });
+
+    it('should update existing functions with changes', async () => {
+        let functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select '${param1}' as param1, '${param2}' as param2, 'changed' as param3`);
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        let proxy = await PGProxy.create(functions, {client: client, schema: 'test'});
+        let results = await client.query(`select proname, prosrc from pg_proc where proname like 'pgproxy_%'`);
+        let procs = results.rows;
+        let found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_select_test');
+        assert.notEqual(found, null);
+
+        found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_system_name');
+        assert.notEqual(found, null);
+
+        let row = await proxy.select_test('asdf','1234');
+        assert.equal(row.param1, 'asdf');
+        assert.equal(row.param2, '1234');
+
+        functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select 'pass!' as param1, '${param2}' as param2, 'changed' as param3`);
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        proxy = await PGProxy.create(functions, {client: client, schema: 'test'});
+        row = await proxy.select_test('asdf','1234');
+        assert.equal(row.param1, 'pass!');
+        assert.equal(row.param2, '1234');
+
+
+    });
+
+    it('should fail to execute changed functions that are not updated', async () => {
+        let functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select '${param1}' as param1, '${param2}' as param2`);
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        let proxy = await PGProxy.create(functions, {client: client, schema: 'test'});
+        let results = await client.query(`select proname, prosrc from pg_proc where proname like 'pgproxy_%'`);
+        let procs = results.rows;
+        let found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_select_test');
+        assert.notEqual(found, null);
+
+        found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_system_name');
+        assert.notEqual(found, null);
+
+        functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select '${param1}' as param1, '${param2}' as param2, 'changed' as param3`);
+                plv8.elog(NOTICE, "I wanna cookie");
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        proxy = await PGProxy.create(functions, {
+            update_changed: false,
+            client: client,
+            schema: 'test'
+        });
+
+        try {
+            let result = await proxy.select_test('nope', 'I said nope!');
+            assert.ok(false); //should not reach here
+        }
+        catch(err) {
+            assert.ok(true); 
+        }
+
+        let name = await proxy.system_name("This is A TEST");
+        assert.equal(name, 'this_is_a_test');
+
+    });
+
+    it('should purge unwanted functions', async () => {
+        let functions = {
+            select_test: (param1, param2) => {
+                let result = plv8.execute(`select '${param1}' as param1, '${param2}' as param2, 'changed' as param3`);
+                return result[0];
+            },
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        let proxy = await PGProxy.create(functions, {client: client, schema: 'test'});
+        let results = await client.query(`select proname, prosrc from pg_proc where proname like 'pgproxy_%'`);
+        let procs = results.rows;
+        let found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_select_test');
+        assert.notEqual(found, null);
+
+        found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_system_name');
+        assert.notEqual(found, null);
+
+        functions = {
+            system_name: (name) => {
+                return name
+                    .toLowerCase()
+                    .replace(/[^a-zA-Z ]/g, '')
+                    .replace(/ /g, '_');
+            }
+        };
+        proxy = await PGProxy.create(functions, {client: client, schema: 'test', purge_other: true});
+        results = await client.query(`select proname, prosrc from pg_proc where proname like 'pgproxy_%'`);
+        procs = results.rows;
+        found = null;
+        found = procs.find((proc) => proc.proname == 'pgproxy_select_test');
+        assert.equal(found, null);
+
+        try {
+            let result = await proxy.select_test('nope', 'I said nope!');
+            assert.ok(false); //should not reach here
+        }
+        catch(err) {
+            assert.ok(true); 
+        }
+    });
+
+
+    after(async () => {
+        await client.end();
+    })
+});
